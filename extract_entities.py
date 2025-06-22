eca810f5d4ad0f64f17391d55ef6ee9740a8f138
Argyris <argybale@gmail.com>
2025-06-22 17:48:29 +0300

    Improve formatting

--- extract_entities.py
+++ extract_entities.py
@@ -1,16 +1,17 @@
 from slither.slither import Slither
-slither=Slither("SmartLease_Full_Compact.sol")
-triples=set()
-for contract in slither.contracts: 
-    triples.add((contract.name, "rdf:type", "SmartContract"))
-    for function in contract.functions_declared: 
-        triples.add ((contract.name,"hasFunction", function.name))
-    for var in contract.state_variables:
-            triples.add((contract.name,"hasVariable", var.name))
-    
-for triple in sorted (triples):
-        print (triple)
 
-with open("triples_output.txt","w",encoding="utf-8") as f:
-      for triple in sorted (triples):
-             f.write(f"{triple}\n")
+slither = Slither("SmartLease.sol")
+triples = set()
+for contract in slither.contracts:
+    triples.add((contract.name, "rdf:type", "SmartContract"))
+    for function in contract.functions_declared:
+        triples.add((contract.name, "hasFunction", function.name))
+    for var in contract.state_variables:
+        triples.add((contract.name, "hasVariable", var.name))
+
+for triple in sorted(triples):
+    print(triple)
+
+with open("triples_output.txt", "w", encoding="utf-8") as f:
+    for triple in sorted(triples):
+        f.write(f"{triple}\n")
